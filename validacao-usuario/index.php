b0f2c1d06ba621c4d30052316a634274005d4302
Jessica <eujeca@hotmail.com>
2026-01-27 09:16:54 -0300

    Completed!

--- validacao-usuario/index.php
+++ validacao-usuario/index.php
@@ -12,11 +12,9 @@ function validarUsuario(?string $nome, ?int $idade, ?string $email)
     if ($nome === '' || $idade < 0 || $email === '') {
         return "Dados vazios ou idade menor do que zero";
     }
-
     if($idade < 18) {
         return "Usuario menor de idade";
     }
-
     return "Tudo certo";
 }
 
