003ec5e11da53d2edeb9d7dc2fc43cf5a62b49ce
Jessica <eujeca@hotmail.com>
2026-01-27 09:06:27 -0300

    Completed!

--- analise_texto/index.php
+++ analise_texto/index.php
@@ -1,2 +1,27 @@
 <?php
+$Texto = null;
 
+function validarTexto(? string $texto) {
+    if ($texto === null) {
+        return 'Texto inválido!';
+}
+
+$textoLimpo = trim(strip_tags($texto));
+
+    if ($textoLimpo === ''){
+        return 'Texto Inválido!';
+    }
+
+    $tamanho = mb_strlen($textoLimpo);
+
+    if ($tamanho <10) {
+        return 'Texto muito curto!';
+    } elseif ($tamanho <= 200) {
+        return 'Texto válido!';
+    }
+    return 'Texto muito longo';
+}
+
+echo validarTexto($Texto);
+
+?>
