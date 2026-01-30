4bb94a1c95be452fef9057a9ee49d0a8359089b4
Jessica <eujeca@hotmail.com>
2026-01-30 19:21:31 -0300

    Completed!

---
+++ calculo_desconto/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$valorProduto = null;
+$Quantidade = null;
+
+function calculoDesconto(?float $valorProduto, ?int $quantidade)
+{
+    if ($quantidade === null || $valorProduto === null) {
+        return "Dados inválidos!";
+    }
+    if ($quantidade <= 0 || $valorProduto <= 0) {
+        return " Valores inválidos!";
+    }
+
+    $total = $valorProduto * $quantidade;
+
+    if ($total >= 200) {
+        $desconto = 0.15;
+    } elseif ($total >= 100) {
+        $desconto = 0.10;
+    } else {
+        $desconto = 0;
+    }
+
+    $valorFinal = $total - ($total * $desconto);
+
+    return "Total: R$ $total | Desconto: " . ($desconto * 100) . "% | Valor Final: R$ $valorFinal";
+}
+
+echo calculoDesconto($valorProduto, $Quantidade);
