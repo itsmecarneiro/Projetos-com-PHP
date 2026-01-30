4d308217027b32589c1d74d0d6750cd817bbfa39
Jessica <eujeca@hotmail.com>
2026-01-30 20:53:20 -0300

    Completed!

---
+++ senha_forte/index.php
@@ -0,0 +1,24 @@
+<?php
+$senha = null;
+
+function validarSenha(?string $senha): string
+{
+    if ($senha === null) {
+        return "Senha inválida!";
+    }
+    $senha = trim($senha);
+    if ($senha === "") {
+        return "Senha vazia!";
+    }
+    if (mb_strlen($senha) < 8) {
+        return "Senha muito curta!";
+    }
+    if (!preg_match('/[A-Z]/', $senha)) {
+        return "Necessário ter pelo menos 1 letra maiúscula!";
+    }
+    if (!preg_match('/[0-9]/', $senha)) {
+        return "Necessário ter pelo menos 1 número!";
+    }
+    return "Senha válida!";
+}
+echo validarSenha($senha);
